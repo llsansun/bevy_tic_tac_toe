c56a0951960c6c39d82ad2800d9ec4bbf7bb23a3
ChatGPT <chatgpt@openai.com>
2023-06-06 22:20:09 +0800

    Added basic game logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 use bevy::prelude*;
 
+[derive(Copy, Clone, Eq, PartialEq)
+enum Player {
+    Cross,
+    Circle,
+}
+
+[derive(Copy, Clone, Eq, PartialEq)]
+enum State {
+    Empty,
+    Occupied(Player),
+}
+
+struct GameBoard([[State; 3]; 3]);
+
+impl Default for GameBoard {
+    fn default() -> Self {
+        Self([[State::Empty; 3]; 3])
+    }
+}
+
 fun main() {
     App::build()
         .add_plugins(DefaultPlugins)
